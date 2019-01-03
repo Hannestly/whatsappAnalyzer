ec31bc05af352fefd9165c0cb8dc3bcee56b3eea
Hannestly <hyon.lee@uoit.net>
2019-01-03 14:31:23 -0500

    refactored to be used as a library

--- whatsapp.py
+++ whatsapp.py
@@ -3,42 +3,21 @@ from collections import Counter
 import os
 import datetime
 
-# Regex complile list
-re_date = re.compile(r'(\d{4}-\d+-\d+).*')
-re_time = re.compile(r'\d{4}-\d+-\d+, (\d+:\d+)')
-re_hour = re.compile(r'^\d+-\d+-\d+, (\d+):')
-re_sender = re.compile(r'\d+-\d+-\d+, \d+:\d+ - (.*?):')
-re_content = re.compile(r'\d{4}-\d+-\d+, \d+:\d+ - \D+: (.*)')
-re_year = re.compile(r'^(\d{4})-')
-re_month = re.compile(r'^\d{4}-(\d+)-')
-re_day = re.compile(r'^\d{4}-\d+-(\d+)')
-
-
-def readfile(filename):
-    """returns textfile in strings"""
-    with open(filename, encoding="utf8") as textfile:
-        data = textfile.read()
-        return data
-
-
-def parse_avoid_file(filename):
-    with open(filename, 'r') as avoidfile:
-        filedata = avoidfile.read()
-        avoid_words = re.split(r',|\s|\n|;', filedata)
-        avoid_words = list(map(lambda x: x.strip(), avoid_words))
-        avoid_words = [word for word in avoid_words if word != '']
-        return avoid_words
-
 
 class messageUnit():
-    def __init__(self, message):
+    def __init__(self, message, avoid_list):
         self.date = None        # date sent
         self.time = None        # time sent
         self.sender = None      # sender
         self.content = None     # message raw content
         self.wordlist = []      # cleaned list of words from raw content
         self.hour = None
+        self.avoid_list = avoid_list
 
+        # Unused Regex compilers
+        # re_year = re.compile(r'^(\d{4})-')
+        # re_month = re.compile(r'^\d{4}-(\d+)-')
+        # re_day = re.compile(r'^\d{4}-\d+-(\d+)')
         self.initmsg(message)
 
     def initmsg(self, message):
@@ -52,36 +31,70 @@ class messageUnit():
             pass
 
     def setDate(self, message):
+        re_date = re.compile(r'(\d{4}-\d+-\d+).*')
         self.date = re_date.match(message)[1]
 
     def setTime(self, message):
+        re_time = re.compile(r'\d{4}-\d+-\d+, (\d+:\d+)')
         self.time = re_time.match(message)[1]
 
     def setSender(self, message):
+        re_sender = re.compile(r'\d+-\d+-\d+, \d+:\d+ - (.*?):')
         self.sender = re_sender.match(message)[1]
 
     def setHour(self, message):
+        re_hour = re.compile(r'^\d+-\d+-\d+, (\d+):')
         self.hour = re_hour.match(message)[1]
 
     def setContent(self, message):
+        re_content = re.compile(r'\d{4}-\d+-\d+, \d+:\d+ - \D+: (.*)')
         re_text = re.compile(r'([a-z\'<>]*)')
         self.content = re_content.match(message)[1]
         tempwordlist = self.content.split(" ")
         tempwordlist = [x.lower() for x in tempwordlist]
         tempwordlist = [x.strip() for x in tempwordlist]
         tempwordlist = [re_text.match(x)[1] for x in tempwordlist]
-        tempwordlist = [x for x in tempwordlist if x not in avoid_words]
+        tempwordlist = [x for x in tempwordlist if x not in self.avoid_list]
         self.wordlist = [x for x in tempwordlist if x is not '']
 
 
 class ChatBank():
-    def __init__(self, chatname):
+    def __init__(self, chatname, avoid_file=None):
         self.chatname = chatname    # name of the chat
+
+        self.message_index = 0
+        self.chatdata = self.read_chatfile(chatname)    # chat data in string
+        self.avoid_list = []        # list of words to be avoided
+        if avoid_file is not None:
+            self.avoid_list = self.parse_avoid_file(avoid_file)
         self.usedwords = Counter()  # used words with counter
         self.users = Counter()      # Chat users with counter
         self.firstday = ""          # first day of the chat
         self.chathours = Counter()  # chatting hours with counter
         self.userwords = {}
+        self.iterate_messages(self.chatdata)
+
+    def read_chatfile(self, chatname):
+        with open(chatname, encoding="utf8") as textfile:
+            data = textfile.read()
+            return data
+
+    def parse_avoid_file(self, avoid_file):
+        with open(avoid_file, 'r') as avoidfile:
+            filedata = avoidfile.read()
+            avoid_words = re.split(r',|\s|\n|;', filedata)
+            avoid_words = list(map(lambda x: x.strip(), avoid_words))
+            avoid_words = [word for word in avoid_words if word != '']
+            return avoid_words
+
+    def iterate_messages(self, chatdata):
+        chatmessages = chatdata.split('\n')
+        for message in chatmessages:
+            msg = messageUnit(message, self.avoid_list)
+            if self.message_index == 0:
+                self.set_firstdate(msg)
+            self.add_tobank(msg)
+            self.message_index += 1
 
     def add_tobank(self, message):
         if message.wordlist is not "":
@@ -118,17 +131,3 @@ class ChatBank():
 
     def set_firstdate(self, message):
         self.firstday = message.date
-
-
-avoid_words = parse_avoid_file(r'avoid_list.txt')
-data = readfile(r'messages.txt')
-chat = data.split('\n')
-
-ChatBank = ChatBank("placeholder")     # Edit the name of the chat
-msgindex = 0
-for messages in chat:
-    message = messageUnit(messages)
-    if msgindex == 0:                   # first entry recorded as first day
-        ChatBank.set_firstdate(message)
-    ChatBank.add_tobank(message)
-    msgindex += 1
